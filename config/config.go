43e88f2f442b17d39b7d3045cc7f5a5628839a69
Kristian H.T <kristian@kristianht.com>
2020-11-17 22:30:08 +0100

    Config models.

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+// AppConfig is the configuration for the service
+type AppConfig struct {
+}
+
+// FileStoreConfig contains the configuration for storing data in files on disk.
+type FileStoreConfig struct {
+}
+
+// AccessControlConfig defines the requirements for an incoming request to be handled.
+type AccessControlConfig struct {
+}
